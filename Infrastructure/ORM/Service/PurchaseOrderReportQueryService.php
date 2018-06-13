f63f6bf25757429db17776620f55b56b59a5cd0c
Pachara Tinamas <mrpachara@gmail.com>
2018-06-14 04:23:13 +0700

    add purchase-order filter boq, budgetType

--- Infrastructure/ORM/Service/PurchaseOrderReportQueryService.php
+++ Infrastructure/ORM/Service/PurchaseOrderReportQueryService.php
@@ -83,6 +83,18 @@ class PurchaseOrderReportQueryService implements QueryInterface
                 ->setParameter('project', $filter['project'])
             ;
         }
+        if(!empty($filter['boq'])) {
+            $qb
+                ->andWhere('_entity_boq = :boq')
+                ->setParameter('boq', $filter['boq'])
+            ;
+        }
+        if(!empty($filter['budgetType'])) {
+            $qb
+                ->andWhere('_entity_budgetType = :budgetType')
+                ->setParameter('budgetType', $filter['budgetType'])
+            ;
+        }
 
         return $qb->getQuery()->getArrayResult();
 
