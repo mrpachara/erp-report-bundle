cf18008a1096892e6a8265ef1fd2ea5813233d41
Pachara Tinamas <mrpachara@gmail.com>
2019-05-03 00:08:11 +0700

    add excel export

--- Controller/PurchaseRequestReportApiQueryController.php
+++ Controller/PurchaseRequestReportApiQueryController.php
@@ -4,7 +4,10 @@ namespace Erp\Bundle\ReportBundle\Controller;
 
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Psr\Http\Message\ServerRequestInterface;
-
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 
 /**
  * PurchaseRequest Report Api Controller
@@ -73,7 +76,7 @@ class PurchaseRequestReportApiQueryController
     /**
      * @Rest\Get("/export.{format}")
      */
-    public function purchaseRequestSummaryExportAction(ServerRequestInterface $request)
+    public function purchaseRequestSummaryExportAction(ServerRequestInterface $request, string $format)
     {
         $filterDetail = [];
         $data = $this->domainQuery->purchaseRequestSummary($request->getQueryParams(), $filterDetail);
@@ -85,16 +88,51 @@ class PurchaseRequestReportApiQueryController
             $logo = stream_get_contents($this->fileQuery->get($profile['logo'])->getData());
         }
         
-        $view = $this->templating->render('@ErpReport/pdf/purchase-request-report.pdf.twig', [
-            'profile' => $profile,
-            'model' => $data,
-            'filterDetail' => $filterDetail,
-        ]);
-        
-        $output = $this->pdfService->generatePdf($view, ['format' => 'A4'], function($mpdf) use ($logo) {
-            $mpdf->imageVars['logo'] = $logo;
-        });
-            
-            return new \TFox\MpdfPortBundle\Response\PDFResponse($output);
+        switch(strtolower($format)) {
+            case 'pdf':
+                $view = $this->templating->render('@ErpReport/pdf/purchase-request-report.pdf.twig', [
+                'profile' => $profile,
+                'model' => $data,
+                'filterDetail' => $filterDetail,
+                ]);
+                
+                $output = $this->pdfService->generatePdf($view, ['format' => 'A4'], function($mpdf) use ($logo) {
+                    $mpdf->imageVars['logo'] = $logo;
+                });
+                    
+                return new \TFox\MpdfPortBundle\Response\PDFResponse($output);
+            case 'xlsx':
+                $spreadsheet = new Spreadsheet();
+                $sheet = $spreadsheet->getActiveSheet();
+                //$sheet->setCellValue('A1', 'Hello World !');
+                $row = 1;
+                $count = 1;
+                foreach($data as $item) {
+                    $sheet->setCellValue('A'.$row, $item['code']);
+                    $sheet->setCellValue('B'.$row, $item['approved']);
+                    $sheet->setCellValue('C'.$row, $item['project']);
+                    $sheet->setCellValue('D'.$row, $item['boq']);
+                    $sheet->setCellValue('E'.$row, $item['budgetType']);
+                    $sheet->setCellValue('F'.$row, $item['requester']);
+                    $sheet->setCellValue('G'.$row, $item['vendor']);
+                    
+                    $row++;
+                    $count++;
+                }
+                
+                // Create your Office 2007 Excel (XLSX Format)
+                $writer = new Xlsx($spreadsheet);
+                
+                // Create a Temporary file in the system
+                $fileName = 'pr-report-'.date('Ymd_His', time()).'.xlsx';
+                $temp_file = tempnam(sys_get_temp_dir(), $fileName);
+                
+                // Create the excel file in the tmp directory of the system
+                $writer->save($temp_file);
+                
+                $response = new BinaryFileResponse($temp_file);
+                $response->setContentDisposition(ResponseHeaderBag::DISPOSITION_INLINE, null === $fileName ? $response->getFile()->getFilename() : $fileName);
+                return $response;
+        }
     }
 }
