ac54e586781426eb28e421b801951afac2919f7a
Anucha <anucha.31con@gmail.com>
2021-09-22 03:12:31 +0700

    update template

--- Controller/PurchaseOrderReportApiQueryController.php
+++ Controller/PurchaseOrderReportApiQueryController.php
@@ -78,7 +78,7 @@ class PurchaseOrderReportApiQueryController
     /**
      * @Rest\Get("/export.{format}")
      */
-    public function purchaseOrderSummaryExportAction(ServerRequestInterface $request, $format)
+    public function purchaseOrderSummaryExportAction(ServerRequestInterface $request, string $format)
     {
         $filterDetail = [];
         $data = $this->domainQuery->purchaseOrderSummary($request->getQueryParams(), $filterDetail);
@@ -99,64 +99,65 @@ class PurchaseOrderReportApiQueryController
                     $mpdf->imageVars['logo'] = $logo;
                 });
                 return new \TFox\MpdfPortBundle\Response\PDFResponse($output);
+            break;
             case 'xlsx':
                 $spreadsheet = new Spreadsheet();
                 $sheet = $spreadsheet->getActiveSheet();
-                $sheet->mergeCells("A1:H1");
+				$sheet->mergeCells("A1:H1");
+                $sheet->mergeCells("C2:H2");
+                $sheet->mergeCells("C3:H3");
+                $sheet->mergeCells("C4:H4");
+                $sheet->mergeCells("C5:H5");
+                $sheet->mergeCells("C6:H6");
                 $sheet->getStyle('A1:H1')->getAlignment()->setHorizontal('center');
-                $sheet->setCellValue('A1', 'รายงานเอกสารใบสั่งซื้อ PO');
-                $sheet->getStyle('A2:A8')->getAlignment()->setHorizontal('right');
-                $sheet->setCellValue('A2', 'โครงการ : ');
-                $sheet->mergeCells("B2:H2");
-                $sheet->setCellValue('B2', (!isset($filterDetail['project']))? 'ทั้งหมด' : "[{$filterDetail['project']->getCode()}] {$filterDetail['project']->getName()}");
-                $sheet->setCellValue('A3', 'งบประมาณ : ');
-                $sheet->mergeCells("B3:H3");
-                $sheet->setCellValue('B3', (!isset($filterDetail['boq']))? 'ทั้งหมด' : "{$filterDetail['boq']->getName()}");
-                $sheet->setCellValue('A4', 'ประเภท : ');
-                $sheet->mergeCells("B4:H4");
-                $sheet->setCellValue('B4', (!isset($filterDetail['budgetType']))? 'ทั้งหมด' : "{$filterDetail['budgetType']->getName()}");
-                $sheet->setCellValue('A5', 'ผู้ต้องการ : ');
-                $sheet->mergeCells("B5:H5");
-                $sheet->setCellValue('B5', (!isset($filterDetail['requester']))? 'ทั้งหมด' : "[{$filterDetail['requester']->getCode()}] {$filterDetail['requester']->getName()}");
-                $sheet->setCellValue('A6', 'ผู้จำหน่าย : ');
-                $sheet->mergeCells("B6:H6");
-                $sheet->setCellValue('B6', (!isset($filterDetail['vendor']))? 'ทั้งหมด' : "[{$filterDetail['vendor']->getCode()}] {$filterDetail['vendor']->getName()}");
-                $sheet->setCellValue('A7', 'สถานะ : ');
-                $sheet->setCellValue('B7', (!isset($filterDetail['approved']))? 'ทั้งหมด' : ($filterDetail['approved']? 'อนุมัติ' : 'รออนุมัติ'));
+                $sheet->getStyle('B2:B8')->getAlignment()->setHorizontal('right');
                 $sheet->getStyle('G7:G8')->getAlignment()->setHorizontal('right');
+                $sheet->getStyle('H7:H8')->getNumberFormat()->setFormatCode('DD/MM/YYYY');
+                $sheet->setCellValue('A1', 'รายงานเอกสารใบสั่งซื้อ (PO-DC)');
+                $sheet->setCellValue('B2', 'โครงการ : ');
+                $sheet->setCellValue('B3', 'งบประมาณ : ');
+                $sheet->setCellValue('B4', 'ประเภท : ');
+                $sheet->setCellValue('B5', 'ผู้ต้องการ : ');
+                $sheet->setCellValue('B6', 'ผู้จำหน่าย : ');
+                $sheet->setCellValue('B7', 'สถานะเอกสาร : ');
+                // $sheet->setCellValue('B8', 'สถานะVAT : ');
                 $sheet->setCellValue('G7', 'วันที่เริ่มต้น : ');
-                $sheet->getStyle('H7')->getNumberFormat()->setFormatCode('DD/MM/YYYY');
-                $sheet->setCellValue('H7', (!isset($filterDetail['start']))? 'ทั้งหมด' : Date::PHPToExcel($filterDetail['start']));
                 $sheet->setCellValue('G8', 'วันที่สิ้นสุด : ');
-                $sheet->getStyle('H8')->getNumberFormat()->setFormatCode('DD/MM/YYYY');
+                $sheet->setCellValue('C2', (!isset($filterDetail['project']))? 'ทั้งหมด' : "[{$filterDetail['project']->getCode()}] {$filterDetail['project']->getName()}");
+                $sheet->setCellValue('C3', (!isset($filterDetail['boq']))? 'ทั้งหมด' : "{$filterDetail['boq']->getName()}");
+                $sheet->setCellValue('C4', (!isset($filterDetail['budgetType']))? 'ทั้งหมด' : "{$filterDetail['budgetType']->getName()}");
+                $sheet->setCellValue('C5', (!isset($filterDetail['requester']))? 'ทั้งหมด' : "[{$filterDetail['requester']->getCode()}] {$filterDetail['requester']->getName()}");
+                $sheet->setCellValue('C6', (!isset($filterDetail['vendor']))? 'ทั้งหมด' : "[{$filterDetail['vendor']->getCode()}] {$filterDetail['vendor']->getName()}");
+                $sheet->setCellValue('C7', (!isset($filterDetail['approved']))? 'ทั้งหมด' : ($filterDetail['approved']? 'อนุมัติ' : 'รออนุมัติ'));
+                // $sheet->setCellValue('C8', (!isset($filterDetail['vatFactor']))? 'ทั้งหมด' : ($filterDetail['vatFactor']? 'มีVAT' : 'ไม่มีVAT'));
+                $sheet->setCellValue('H7', (!isset($filterDetail['start']))? 'ทั้งหมด' : Date::PHPToExcel($filterDetail['start']));
                 $sheet->setCellValue('H8', (!isset($filterDetail['end']))? 'ทั้งหมด' : Date::PHPToExcel($filterDetail['end']));
-                $sheet->getStyle('A9:H10')->getBorders()->getAllBorders()->setBorderStyle(\PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN);
+                $sheet->mergeCells('A9:A10');
+                $sheet->mergeCells('B9:C9');
+                $sheet->mergeCells('D9:F9');
+                $sheet->mergeCells('G9:G10');
+                $sheet->mergeCells('H9:H10');
+				$sheet->getStyle('A9:H10')->getBorders()->getAllBorders()->setBorderStyle(\PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN);
                 $sheet->getStyle('A9:H10')->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID);
                 $sheet->getStyle('A9:H10')->getFill()->getStartColor()->setRGB('DCDCDC');
 				$sheet->getStyle('A9:H10')->getAlignment()->setHorizontal('center');
                 $sheet->getStyle('A9:H10')->getAlignment()->setVertical('center');
-                $sheet->mergeCells('A9:A10');
                 $sheet->setCellValue('A9', 'ลำดับ');
-                $sheet->mergeCells('B9:C9');
                 $sheet->setCellValue('B9', 'เอกสาร');
                 $sheet->setCellValue('B10', 'เลขที่');
                 $sheet->setCellValue('C10', 'สถานะ');
-                $sheet->mergeCells('D9:F9');
                 $sheet->setCellValue('D9', 'โครงการ');
                 $sheet->setCellValue('D10', 'รหัส');
                 $sheet->setCellValue('E10', 'งบประมาณ');
                 $sheet->setCellValue('F10', 'ประเภท');
-                $sheet->mergeCells('G9:G10');
                 $sheet->setCellValue('G9', 'ผู้ต้องการ');
-                $sheet->mergeCells('H9:H10');
                 $sheet->setCellValue('H9', 'ผู้จำหน่าย');
-
+				
                 $row = 11;
                 $count = 1;
                 foreach($data as $item) {
-                    $sheet->getStyle('A')->getAlignment()->setHorizontal('center');
-                    $sheet->getStyle('C:D')->getAlignment()->setHorizontal('center');
-                    $sheet->getStyle('G:H')->getAlignment()->setHorizontal('center');
+					$sheet->getStyle('A:D')->getAlignment()->setHorizontal('center');
+                    $sheet->getStyle('F:H')->getAlignment()->setHorizontal('center');
                     $sheet->setCellValue('A'.$row, $count);
                     $sheet->setCellValue('B'.$row, $item['code']);
                     $sheet->setCellValue('C'.$row, $item['approved']? 'อนุมัติ' : 'รออนุมัติ');
@@ -169,14 +170,15 @@ class PurchaseOrderReportApiQueryController
                     $row++;
                     $count++;
                 }
-                
+				
                 $writer = new Xlsx($spreadsheet);
-                $fileName = 'po-document-report-'.date('Ymd_His', time()).'.xlsx';
+                $fileName = 'RP-DC-PU-PO_rev2.1.0_'.date('Ymd_His', time()).'.xlsx';
                 $temp_file = tempnam(sys_get_temp_dir(), $fileName);
                 $writer->save($temp_file);
                 $response = new BinaryFileResponse($temp_file);
                 $response->setContentDisposition(ResponseHeaderBag::DISPOSITION_INLINE, null === $fileName ? $response->getFile()->getFilename() : $fileName);
                 return $response;
+            break;
         }
     }
 
@@ -204,38 +206,40 @@ class PurchaseOrderReportApiQueryController
                     $mpdf->imageVars['logo'] = $logo;
                 });
                 return new \TFox\MpdfPortBundle\Response\PDFResponse($output);
+            break;
             case 'xlsx':
                 $costFormat = '#,##0.00_-;[Red]-#,##0.00_-;??"-"??_-;[Green]@_-';
 
                 $spreadsheet = new Spreadsheet();
                 $sheet = $spreadsheet->getActiveSheet();
                 $sheet->mergeCells("A1:K1");
+                $sheet->mergeCells("C2:K2");
+                $sheet->mergeCells("C3:K3");
+                $sheet->mergeCells("C4:K4");
+                $sheet->mergeCells("C5:K5");
+                $sheet->mergeCells("C6:K6");
                 $sheet->getStyle('A1:K1')->getAlignment()->setHorizontal('center');
-                $sheet->setCellValue('A1', 'รายงานมูลค่าการสั่งซื้อ PO');
-                $sheet->getStyle('A2:A8')->getAlignment()->setHorizontal('right');
-                $sheet->setCellValue('A2', 'โครงการ : ');
-                $sheet->mergeCells("B2:K2");
-                $sheet->setCellValue('B2', (!isset($filterDetail['project']))? 'ทั้งหมด' : "[{$filterDetail['project']->getCode()}] {$filterDetail['project']->getName()}");
-                $sheet->setCellValue('A3', 'งบประมาณ : ');
-                $sheet->mergeCells("B3:K3");
-                $sheet->setCellValue('B3', (!isset($filterDetail['boq']))? 'ทั้งหมด' : "{$filterDetail['boq']->getName()}");
-                $sheet->setCellValue('A4', 'ประเภท : ');
-                $sheet->mergeCells("B4:K4");
-                $sheet->setCellValue('B4', (!isset($filterDetail['budgetType']))? 'ทั้งหมด' : "{$filterDetail['budgetType']->getName()}");
-                $sheet->setCellValue('A5', 'ผู้ต้องการ : ');
-                $sheet->mergeCells("B5:K5");
-                $sheet->setCellValue('B5', (!isset($filterDetail['requester']))? 'ทั้งหมด' : "[{$filterDetail['requester']->getCode()}] {$filterDetail['requester']->getName()}");
-                $sheet->setCellValue('A6', 'ผู้จำหน่าย : ');
-                $sheet->mergeCells("B6:K6");
-                $sheet->setCellValue('B6', (!isset($filterDetail['vendor']))? 'ทั้งหมด' : "[{$filterDetail['vendor']->getCode()}] {$filterDetail['vendor']->getName()}");
-                $sheet->setCellValue('A7', 'สถานะ : ');
-                $sheet->setCellValue('B7', (!isset($filterDetail['approved']))? 'ทั้งหมด' : ($filterDetail['approved']? 'อนุมัติ' : 'รออนุมัติ'));
+                $sheet->getStyle('B2:B8')->getAlignment()->setHorizontal('right');
                 $sheet->getStyle('J7:J8')->getAlignment()->setHorizontal('right');
+                $sheet->getStyle('K7:K8')->getNumberFormat()->setFormatCode('DD/MM/YYYY');
+                $sheet->setCellValue('A1', 'รายงานมูลค่าใบสั่งซื้อ (PO-FI)');
+                $sheet->setCellValue('B2', 'โครงการ : ');
+                $sheet->setCellValue('B3', 'งบประมาณ : ');
+                $sheet->setCellValue('B4', 'ประเภท : ');
+                $sheet->setCellValue('B5', 'ผู้ต้องการ : ');
+                $sheet->setCellValue('B6', 'ผู้จำหน่าย : ');
+                $sheet->setCellValue('B7', 'สถานะเอกสาร : ');
+                // $sheet->setCellValue('B8', 'สถานะVAT : ');
                 $sheet->setCellValue('J7', 'วันที่เริ่มต้น : ');
-                $sheet->getStyle('K7')->getNumberFormat()->setFormatCode('DD/MM/YYYY');
-                $sheet->setCellValue('K7', (!isset($filterDetail['start']))? 'ทั้งหมด' : Date::PHPToExcel($filterDetail['start']));
                 $sheet->setCellValue('J8', 'วันที่สิ้นสุด : ');
-                $sheet->getStyle('K8')->getNumberFormat()->setFormatCode('DD/MM/YYYY');
+                $sheet->setCellValue('C2', (!isset($filterDetail['project']))? 'ทั้งหมด' : "[{$filterDetail['project']->getCode()}] {$filterDetail['project']->getName()}");
+                $sheet->setCellValue('C3', (!isset($filterDetail['boq']))? 'ทั้งหมด' : "{$filterDetail['boq']->getName()}");
+                $sheet->setCellValue('C4', (!isset($filterDetail['budgetType']))? 'ทั้งหมด' : "{$filterDetail['budgetType']->getName()}");
+                $sheet->setCellValue('C5', (!isset($filterDetail['requester']))? 'ทั้งหมด' : "[{$filterDetail['requester']->getCode()}] {$filterDetail['requester']->getName()}");
+                $sheet->setCellValue('C6', (!isset($filterDetail['vendor']))? 'ทั้งหมด' : "[{$filterDetail['vendor']->getCode()}] {$filterDetail['vendor']->getName()}");
+                $sheet->setCellValue('C7', (!isset($filterDetail['approved']))? 'ทั้งหมด' : ($filterDetail['approved']? 'อนุมัติ' : 'รออนุมัติ'));
+                // $sheet->setCellValue('C8', (!isset($filterDetail['vatFactor']))? 'ทั้งหมด' : ($filterDetail['vatFactor']? 'มีVAT' : 'ไม่มีVAT'));
+                $sheet->setCellValue('K7', (!isset($filterDetail['start']))? 'ทั้งหมด' : Date::PHPToExcel($filterDetail['start']));
                 $sheet->setCellValue('K8', (!isset($filterDetail['end']))? 'ทั้งหมด' : Date::PHPToExcel($filterDetail['end']));
                 $sheet->getStyle('A9:K10')->getBorders()->getAllBorders()->setBorderStyle(\PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN);
                 $sheet->getStyle('A9:K10')->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID);
@@ -243,17 +247,17 @@ class PurchaseOrderReportApiQueryController
 				$sheet->getStyle('A9:K10')->getAlignment()->setHorizontal('center');
                 $sheet->getStyle('A9:K10')->getAlignment()->setVertical('center');
                 $sheet->mergeCells('A9:A10');
-                $sheet->setCellValue('A9', 'ลำดับ');
                 $sheet->mergeCells('B9:C9');
+                $sheet->mergeCells('D9:F9');
+                $sheet->mergeCells('G9:K9');
+                $sheet->setCellValue('A9', 'ลำดับ');
                 $sheet->setCellValue('B9', 'เอกสาร');
                 $sheet->setCellValue('B10', 'เลขที่');
                 $sheet->setCellValue('C10', 'สถานะ');
-                $sheet->mergeCells('D9:F9');
                 $sheet->setCellValue('D9', 'โครงการ');
                 $sheet->setCellValue('D10', 'รหัส');
                 $sheet->setCellValue('E10', 'งบประมาณ');
                 $sheet->setCellValue('F10', 'ประเภท');
-                $sheet->mergeCells('G9:K9');
                 $sheet->setCellValue('G9', 'มูลค่าการสั่งซื้อ');
                 $sheet->setCellValue('G10', 'VAT');
                 $sheet->setCellValue('H10', 'ไม่รวมVAT');
@@ -318,7 +322,7 @@ class PurchaseOrderReportApiQueryController
                 $writer->setPreCalculateFormulas(false);
 
                 // Create a Temporary file in the system
-                $fileName = 'po-cost-report-'.date('Ymd_His', time()).'.xlsx';
+                $fileName = 'RP-FI-PU-PO_rev2.1.0_'.date('Ymd_His', time()).'.xlsx';
                 $temp_file = tempnam(sys_get_temp_dir(), $fileName);
                 
                 // Create the excel file in the tmp directory of the system
@@ -327,7 +331,7 @@ class PurchaseOrderReportApiQueryController
                 $response = new BinaryFileResponse($temp_file);
                 $response->setContentDisposition(ResponseHeaderBag::DISPOSITION_INLINE, null === $fileName ? $response->getFile()->getFilename() : $fileName);
                 return $response;
-            }
-
+            break;
+        }
     }
 }
